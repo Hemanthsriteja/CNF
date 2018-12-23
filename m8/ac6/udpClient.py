e74fa5311740e7aec56b1e5fd39e55946e0dd186
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-12-23 19:56:56 +0530

    ac6

---
+++ m8/ac6/udpClient.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def main():
+	host = '10.1.134.217'
+	port = 5001
+
+
+	server=('10.1.134.217',5000)
+
+	s= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host,port))
+
+	message = input("Enter message:  ")
+	while message !='q':
+		s.sendto(str(message).encode('ASCII'),server)
+		data, addr = s.recvfrom(1024)
+		print("Recieved from server: "+ data.decode('ASCII'))
+		message = input("Enter another message: ")
+	s.close()
+
+if __name__=='__main__':
+ main()
